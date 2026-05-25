1a90052ffd9f9661c23bde0e7885c289f81b749c
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-05-24 20:36:08 -0400

    holy fucking shit the spine animation loads now

--- AveMujicaCode/Character/AveMujica.cs
+++ AveMujicaCode/Character/AveMujica.cs
@@ -3,6 +3,8 @@ using BaseLib.Utils.NodeFactories;
 using AveMujica.AveMujicaCode.Extensions;
 using BaseLib.Utils;
 using Godot;
+using MegaCrit.Sts2.Core.Animation;
+using MegaCrit.Sts2.Core.Bindings.MegaSpine;
 using MegaCrit.Sts2.Core.Entities.Characters;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Cards;
@@ -56,6 +58,31 @@ public class AveMujica : PlaceholderCharacterModel
             icon.SetAnchorsAndOffsetsPreset(Control.LayoutPreset.FullRect);
             return icon;
         }
+    }
+    
+    public override CreatureAnimator GenerateAnimator(MegaSprite controller)
+    {
+        AnimState animState = new AnimState("Idle", isLooping: true);
+        // AnimState animState2 = new AnimState("cast");
+        // AnimState animState3 = new AnimState("attack");
+        // AnimState animState4 = new AnimState("hurt");
+        // AnimState state = new AnimState("die");
+        // AnimState animState5 = new AnimState("shiv");
+        // AnimState animState6 = new AnimState("relaxed_loop", isLooping: true);
+        // animState2.NextState = animState;
+        // animState3.NextState = animState;
+        // animState4.NextState = animState;
+        // animState5.NextState = animState;
+        // animState6.AddBranch("Idle", animState);
+        CreatureAnimator creatureAnimator = new CreatureAnimator(animState, controller);
+        creatureAnimator.AddAnyState("Idle", animState);
+        // creatureAnimator.AddAnyState("Dead", state);
+        // creatureAnimator.AddAnyState("Hit", animState4);
+        // creatureAnimator.AddAnyState("Attack", animState3);
+        // creatureAnimator.AddAnyState("Cast", animState2);
+        // creatureAnimator.AddAnyState("Shiv", animState5);
+        // creatureAnimator.AddAnyState("Relaxed", animState6);
+        return creatureAnimator;
     }
     
     public override string CustomVisualPath => "res://AveMujica/images/character/oblvns.tscn";
---
+++ AveMujicaCode/Vfx/AveMujicaNCreatureVisuals.cs
@@ -0,0 +1,6 @@
+using Godot;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+
+namespace AveMujica.AveMujicaCode.Vfx;
+
+public partial class AveMujicaNCreatureVisuals : NCreatureVisuals;
