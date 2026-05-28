adbc6a1b79fb9ec4f774e843e8c50d0806e81beb
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-05-28 18:23:07 -0400

    implement oblivion mechanic

---
+++ AveMujicaCode/Cards/Common/MoonlitStrike.cs
@@ -0,0 +1,36 @@
+﻿using AveMujica.AveMujicaCode.Powers;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace AveMujica.AveMujicaCode.Cards.Common;
+
+public class MoonlitStrike() : AveMujicaCard(1,
+    CardType.Attack, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(7, ValueProp.Move), new PowerVar<Oblivion>(2)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CommonActions.CardAttack(this, play).Execute(choiceContext);
+        await PowerCmd.Apply<Oblivion>(choiceContext, Owner.Creature, DynamicVars["Oblivion"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(2);
+        DynamicVars["Oblivion"].UpgradeValueBy(1);
+    }
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower(ModelDb.Power<Oblivion>())
+    ];
+}
---
+++ AveMujicaCode/Powers/Oblivion.cs
@@ -0,0 +1,39 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace AveMujica.AveMujicaCode.Powers;
+
+public class Oblivion : AveMujicaPower
+{
+    public override PowerType Type =>
+        PowerType.Buff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+    
+    public override async Task AfterPowerAmountChanged(
+        PlayerChoiceContext choiceContext,
+        PowerModel power,
+        Decimal amount,
+        Creature? applier,
+        CardModel? cardSource)
+    {
+        if (Owner.CombatState != null && power is Oblivion && Owner == applier)
+        {
+            IReadOnlyList<Creature> hittableEnemies = Owner.CombatState.HittableEnemies;
+            if (hittableEnemies.Count != 0)
+            {
+                Creature? weakestEnemy = hittableEnemies.MinBy((Func<Creature, int>) (c => c.CurrentHp));
+                if (weakestEnemy != null)
+                {
+                    Flash();
+                    await CreatureCmd.Damage(new BlockingPlayerChoiceContext(), weakestEnemy, Amount, ValueProp.Unpowered, Owner);
+                }
+            }
+        }
+    }
+}
