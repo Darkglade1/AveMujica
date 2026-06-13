93ca17341645b4df2fe39b62322d40c22aa34ef1
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-06-12 22:34:12 -0400

    rework musica caelestis

--- AveMujicaCode/Cards/Rare/MusicaCaelestis.cs
+++ AveMujicaCode/Cards/Rare/MusicaCaelestis.cs
@@ -1,9 +1,12 @@
 ﻿using AveMujica.AveMujicaCode.Cards.Token;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace AveMujica.AveMujicaCode.Cards.Rare;
@@ -12,12 +15,7 @@ public class MusicaCaelestis() : AveMujicaCard(1,
     CardType.Attack, CardRarity.Rare,
     TargetType.AnyEnemy)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new CalculationBaseVar(8),
-        new ExtraDamageVar(4),
-        new CalculatedDamageVar(ValueProp.Move).WithMultiplier((c, _) 
-            => PileType.Exhaust.GetPile(c.Owner).Cards.Count(e => e is Song))
-    ];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [ new DamageVar(8, ValueProp.Move)];
     
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [
         HoverTipFactory.FromKeyword(AveMujicaKeywords.Compose)
@@ -26,7 +24,13 @@ public class MusicaCaelestis() : AveMujicaCard(1,
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         await CommonActions.CardAttack(this, play).Execute(choiceContext);
+        CardSelectorPrefs prefs = new CardSelectorPrefs(SelectionScreenPrompt, 1);
+        CardModel? selection = (await CardSelectCmd.FromCombatPile(choiceContext,  PileType.Exhaust.GetPile(Owner), Owner, prefs, c => c is Song)).FirstOrDefault();
+        if (selection == null)
+            return;
+        await CardCmd.AutoPlay(choiceContext, selection, play.Target);
+        await CardPileCmd.RemoveFromCombat(selection, true);
     }
 
-    protected override void OnUpgrade() => DynamicVars.ExtraDamage.UpgradeValueBy(2);
+    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(3);
 }
