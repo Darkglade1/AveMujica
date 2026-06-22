01039046f11de304239a460baf9cc5a9e84a933f
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-06-22 19:37:24 -0400

    rework waxing moon

--- AveMujicaCode/Cards/Uncommon/WaxingMoonLonging.cs
+++ AveMujicaCode/Cards/Uncommon/WaxingMoonLonging.cs
@@ -1,59 +1,38 @@
 ﻿using AveMujica.AveMujicaCode.Cards.Allies;
+using BaseLib.Patches.Features;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.ValueProps;
 
 namespace AveMujica.AveMujicaCode.Cards.Uncommon;
 
 public class WaxingMoonLonging() : AveMujicaCard(0,
     CardType.Skill, CardRarity.Uncommon,
-    TargetType.Self)
+    CustomTargetType.PetOrSelf)
 {
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Innate, CardKeyword.Exhaust];
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new CalculationBaseVar(0),
-        new CalculationExtraVar(1),
-        new CalculatedBlockVar(ValueProp.Move).WithMultiplier(MultiplierCalc)
+        new DreamspinVar(3)
     ];
-
-    private static decimal MultiplierCalc(CardModel card, Creature? creature)
-    {
-        if (card.Owner.Creature.CombatState == null)
-        {
-            return 0;
-        }
-
-        int totalHP = 0;
-        foreach (var ally in card.Owner.Creature.CombatState.Allies)
-        {
-            if (ally.IsPet && ally.IsAlive && ally.PetOwner == card.Owner && ally.Monster is AbstractAlly)
-            {
-                totalHP += ally.CurrentHp;
-            }
-        }
-        return totalHP;
-    }
-    
-    public override bool GainsBlock => true;
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.CalculatedBlock.Calculate(play.Target), DynamicVars.CalculatedBlock.Props, play);
+        await AllyHelper.Dreamspin(choiceContext, Owner, DynamicVars["Dreamspin"].IntValue, play.Target, this);
+        if (IsUpgraded)
+        {
+            await CardPileCmd.Draw(choiceContext, 1, Owner);
+        }
     }
 
     protected override void OnUpgrade()
     {
-        RemoveKeyword(CardKeyword.Exhaust);
     }
     
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [
-        HoverTipFactory.FromKeyword(AveMujicaKeywords.Doll)
+        HoverTipFactory.FromKeyword(AveMujicaKeywords.Dreamspin)
     ];
 }
