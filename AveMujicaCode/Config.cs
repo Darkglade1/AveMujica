5fa91e4b421f3ce3241977a1f82a61c46d2b6e2d
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-06-25 20:58:47 -0400

    add ftue for dreamspin

--- AveMujicaCode/Config.cs
+++ AveMujicaCode/Config.cs
@@ -4,10 +4,13 @@ namespace AveMujica.AveMujicaCode;
 
 public class Config: SimpleModConfig
 {
-    [ConfigSection("Misc")]
+    [ConfigSection("Skins")]
     public static bool UseOblivionisSkin { get; set; } = false;
     public static bool UseDolorisSkin { get; set; } = false;
     public static bool UseMortisSkin { get; set; } = false;
     public static bool UseTimorisSkin { get; set; } = false;
     public static bool UseAmorisSkin { get; set; } = false;
+    
+    [ConfigSection("Tutorial")]
+    public static bool ViewDreamspinFtue { get; set; } = true;
 }
---
+++ AveMujicaCode/Ftue/DreamspinFtue.cs
@@ -0,0 +1,46 @@
+﻿using AveMujica.AveMujicaCode.Cards;
+using AveMujica.AveMujicaCode.Extensions;
+using BaseLib.Abstracts;
+using Godot;
+using MegaCrit.Sts2.Core.Context;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
+
+namespace AveMujica.AveMujicaCode.Ftue;
+
+public class DreamspinFtue() : CustomSingletonModel(HookType.Combat)
+{
+	public override Task AfterCardDrawn(
+		PlayerChoiceContext choiceContext,
+		CardModel card,
+		bool fromHandDraw)
+	{
+		if (LocalContext.IsMe(card.Owner) &&
+		    card.HoverTips.Contains(HoverTipFactory.FromKeyword(AveMujicaKeywords.Dreamspin)) && Config.ViewDreamspinFtue)
+		{
+			return TaskHelper.RunSafely(ShowCombatFtue());
+		}
+		return Task.CompletedTask;
+	}
+
+	private static async Task ShowCombatFtue()
+	{
+		SceneTree? tree = NModalContainer.Instance != null ? NModalContainer.Instance.GetTree() : null;
+		if (tree == null)
+		{
+			return;
+		}
+		if (((NModalContainer.Instance != null) ? NModalContainer.Instance.OpenModal : null) == null)
+		{
+			NDreamspinFtue nDreamspinFtue = NDreamspinFtue.Create("Dreamspin", ["AVEMUJICA-DREAMSPIN-FTUE.title", "AVEMUJICA-DREAMSPIN-FTUE.title"], ["AVEMUJICA-DREAMSPIN-FTUE.body", "AVEMUJICA-DREAMSPIN-FTUE.body"], ["ftue/dreamspin_ftue.png".ImagePath(), "ftue/dreamspin_ftue2.png".ImagePath()]);
+			if (NModalContainer.Instance != null)
+			{
+				NModalContainer.Instance.Add(nDreamspinFtue);
+				Config.ViewDreamspinFtue = false;
+			}
+		}
+	}
+}
