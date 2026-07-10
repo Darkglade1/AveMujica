6252a6fd5cafaad4cd80027b6af7310e3ba576ac
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-07-09 21:22:00 -0400

    yeet crucifix x and add new two moons in

---
+++ AveMujicaCode/Cards/Rare/TwoMoons.cs
@@ -0,0 +1,46 @@
+﻿using AveMujica.AveMujicaCode.Cards.Dolls;
+using BaseLib.Patches.Features;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace AveMujica.AveMujicaCode.Cards.Rare;
+
+public class TwoMoons() : AveMujicaCard(1,
+    CardType.Skill, CardRarity.Rare,
+    CustomTargetType.PetOrSelf)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new HpLossVar(2)];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromKeyword(AveMujicaKeywords.Dreamspin)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await DollHelper.Dreamspin(choiceContext, Owner, play.Target, this);
+        if (play.Target != null && play.Target.Monster is AbstractDoll)
+        {
+            await SpendHPToDreamspin(choiceContext, play);
+            if (IsUpgraded)
+            {
+                await SpendHPToDreamspin(choiceContext, play);
+            }   
+        }
+    }
+
+    private async Task SpendHPToDreamspin(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        if (play.Target != null && play.Target.CurrentHp >= DynamicVars.HpLoss.BaseValue)
+        {
+            await DollHelper.Dreamspin(choiceContext, Owner, play.Target, this);
+            await CreatureCmd.Damage(choiceContext, play.Target, DynamicVars.HpLoss.BaseValue, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move, Owner.Creature);
+            await CreatureCmd.LoseMaxHp(choiceContext, play.Target, DynamicVars.HpLoss.BaseValue, false);
+        }
+    }
+}
