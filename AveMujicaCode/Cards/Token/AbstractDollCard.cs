a92fc72b7ed7161d40c776a117c1615c54462d4a
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-07-31 15:06:22 -0400

    add doll type text and fix beta

--- AveMujicaCode/Cards/Token/AbstractDollCard.cs
+++ AveMujicaCode/Cards/Token/AbstractDollCard.cs
@@ -1,14 +1,21 @@
 ﻿using AveMujica.AveMujicaCode.Extensions;
+using BaseLib.Abstracts;
 using BaseLib.Extensions;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Localization;
 
 namespace AveMujica.AveMujicaCode.Cards.Token;
 
 public abstract class AbstractDollCard(int cost, CardType type, CardRarity rarity, TargetType target)
-    : AveMujicaCard(cost, type, rarity, target)
+    : AveMujicaCard(cost, type, rarity, target), ICustomTypeTextCard
 {
     public override string CustomPortraitPath => IsSkinEnabled() ? $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_skin.png".BigCardImagePath() : $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
     public override string PortraitPath => IsSkinEnabled() ? $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_skin.png".CardImagePath() : $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
 
     protected abstract bool IsSkinEnabled();
+    public IEnumerable<LocString> GetTypeModifiers()
+    {
+        var dollType = new LocString("static_hover_tips", "AVEMUJICA-DOLL_TYPE.description");
+        return [dollType];
+    }
 }
