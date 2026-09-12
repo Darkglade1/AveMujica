d7acc5075393212fb444b56d228b9f7d7b46e256
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-09-11 21:17:28 -0400

    Add old bottle relic

---
+++ AveMujicaCode/Relics/OldBottle.cs
@@ -0,0 +1,61 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Rooms;
+
+namespace AveMujica.AveMujicaCode.Relics;
+
+public class OldBottle : AveMujicaRelic
+{
+    private bool _wasUsedThisCombat;
+    
+    public override RelicRarity Rarity =>
+        RelicRarity.Uncommon;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(2)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromKeyword(CardKeyword.Exhaust), HoverTipFactory.Static(StaticHoverTip.Energy)];
+    
+    public bool WasUsedThisCombat
+    {
+        get => _wasUsedThisCombat;
+        set
+        {
+            AssertMutable();
+            _wasUsedThisCombat = value;
+        }
+    }
+    
+    public override Task AfterRoomEntered(AbstractRoom room)
+    {
+        if (!(room is CombatRoom))
+            return Task.CompletedTask;
+        WasUsedThisCombat = false;
+        Status = RelicStatus.Active;
+        return Task.CompletedTask;
+    }
+
+    public override async Task AfterCardExhausted(
+        PlayerChoiceContext choiceContext,
+        CardModel card,
+        bool causedByEthereal)
+    {
+        if (card.Owner != Owner || WasUsedThisCombat)
+            return;
+        Flash();
+        await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, Owner);
+        Status = RelicStatus.Normal;
+        WasUsedThisCombat = true;
+    }
+
+    public override Task AfterCombatEnd(CombatRoom _)
+    {
+        WasUsedThisCombat = false;
+        Status = RelicStatus.Normal;
+        return Task.CompletedTask;
+    }
+}
